54ea009786f33c43fc50d00051ce59696aeb8ff0
Joshua Robinson <joshuarobinson@Joshuas-MBP.attlocal.net>
2020-04-29 21:32:04 -0400

    initial commit

---
+++ database/connection.php
@@ -0,0 +1,34 @@
+<?php
+// local using variables
+// $server = "localhost";
+// $user = "wbip";
+// $pw = "wbip123";
+// $db = "test";
+
+// local using constants
+
+if($_SERVER['HTTP_HOST'] == "joshuadrobinson.com")
+{
+    define("SERVER", "localhost");
+    define("USER", "joshua99_wbip");
+    define("PW", "Joshuarob17");
+    define("DB", "joshua99_test");
+
+}
+else {
+define("SERVER", "localhost");
+define("USER", "wbip");
+define("PW", "wbip123");
+define("DB", "test");
+}
+
+// global $connect;
+// $connect = mysqli_connect(SERVER, USER, PW, DB);
+
+
+
+// siteground
+// $server = "localhost";
+// $user = "charl989_admin";
+// $pw = "Gabc";
+// $db = "charl989_test";
